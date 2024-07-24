0c08b6b763e4841dc8e5a850939c98d6917a53af
Toocanzs <dupanzszs@gmail.com>
2024-07-24 02:12:55 -0400

    mend

--- src/main/java/com/toogpu/TooGpuPlugin.java
+++ src/main/java/com/toogpu/TooGpuPlugin.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018, Adam <Adam@sigterm.info>
+ * Copyright (c) 2021, 117 <https://twitter.com/117scape>
+ * Copyright (c) 2024, Toocanzs <https://github.com/Toocanzs>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.toogpu;
 
 import com.google.inject.Provides;
@@ -19,8 +46,11 @@ import org.lwjgl.opengl.*;
 import org.lwjgl.opengl.GLCapabilities;
 import org.lwjgl.system.Callback;
 import org.lwjgl.system.Configuration;
-
-import java.awt.*;
+import java.awt.Canvas;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.lwjgl.opengl.GL43C.*;
 
@@ -85,24 +115,21 @@ public class TooGpuPlugin extends Plugin implements DrawCallbacks
 				log.info("Using driver: {}", glGetString(GL_VERSION));
 				log.info("Client is {}-bit", arch);
 
-				/*computeMode = OSType.getOSType() == OSType.MacOS ? ComputeMode.OPENCL : ComputeMode.OPENGL;
-
 				List<String> fallbackDevices = List.of(
 						"GDI Generic",
 						"D3D12 (Microsoft Basic Render Driver)",
 						"softpipe"
 				);
-				boolean isFallbackGpu = fallbackDevices.contains(glRenderer) && !Props.has("rlhd.allowFallbackGpu");
-				boolean isUnsupportedGpu = isFallbackGpu || (computeMode == ComputeMode.OPENGL ? !glCaps.OpenGL43 : !glCaps.OpenGL31);
+				boolean isFallbackGpu = fallbackDevices.contains(glRenderer);
+				boolean isUnsupportedGpu = isFallbackGpu || (!glCaps.OpenGL43);
 				if (isUnsupportedGpu) {
 					log.error(
-							"The GPU is lacking OpenGL {} support. Stopping the plugin...",
-							computeMode == ComputeMode.OPENGL ? "4.3" : "3.1"
+							"The GPU is lacking OpenGL 4.3 support. Stopping the plugin..."
 					);
-					displayUnsupportedGpuMessage(isFallbackGpu, glRenderer);
+					// TODO: displayUnsupportedGpuMessage(isFallbackGpu, glRenderer);
 					stopPlugin();
 					return true;
-				}*/
+				}
 
 				lwjglInitialized = true;
 				checkGLErrors();
